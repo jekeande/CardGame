0c3cad14c47116a9c87381af3e3aede3a702aaef
Jennifer Ancheta <jekeande@gmail.com>
2022-08-11 17:43:18 -0300

    commit 11/8/22

--- index.js
+++ index.js
@@ -11,36 +11,40 @@ const db = new sqlite3.Database('./data/CardGame.db', (err) => {
     } else {
         console.log("Conectado com o Banco de Dados")
     }
-})
+});
 
 app.post("/card",(req, res, next) => {
     db.run(`INSERT INTO card (name_card,hp,attack,defense,special_attack,special_defense,speed)
-            VALUES(?,?,?,?,?,?,?)`,
-            [
-                req.body.name,
+            VALUES (?,?,?,?,?,?,?)`,
+            [   req.body.name,
                 req.body.hp,
                 req.body.attack,
                 req.body.defense,
                 req.body.special_attack,
                 req.body.special_defense,
-                req.body.speed,
-            ], (err, rows) => {
+                req.body.speed
+            ], function(err, rows){
                 if (err) {
                     res.status(400).json({ error: err.message });
                     return;
-                } else
-                    res.status(201).json({"Card ID": this.lastID});
+                } else {
+                    res.status(201).json({ "Nova Card ID": this.lastID });
+                }
             }
         ); 
-})
+});
 
 app.get("/cards", (req, res, next) => {
-    db.all(`SELECT * FROM card`, [], (err, rows) => {
+    db.all(`SELECT * FROM card`, 
+    [], function(err, rows){
         if (err) {
             res.status(400).json({ "error": err.message });
             return;
-        } else
+        } else if (rows.length > 0) {
             res.status(200).json(rows);
+        } else {
+            res.status(200).json("Banco de dados sem registros");
+        }
     });
 });
 
@@ -48,14 +52,15 @@ app.get("/card", (req, res, next) => {
     db.all(`SELECT * FROM card WHERE id_card=?`,
             [
                 req.body.id_card
-            ], (err, rows) => {
+            ], function(err, rows){
                 if (err) {
                     res.status(400).json({ "error": err.message });
                     return;
-                } else if (rows = undefined || []){
-                    res.status(200).json("Card ID "+ req.body.id_card +" Não existe");
-                } else 
+                } else if (rows.length > 0) {
                     res.status(200).json(rows);
+                } else {
+                    res.status(200).json("Card ID "+ req.body.id_card +" Não existe");
+                }
             });
 });
  
@@ -71,62 +76,136 @@ app.put("/card",(req, res, next) => {
                 req.body.special_defense,
                 req.body.speed,
                 req.body.id_card
-            ], (err, rows) => {
+            ], function(err, rows){
                 if(err) {
                     res.status(400).json({ "error": err.message });
                     return;
-                } else if (rows = undefined || []){
-                    res.status(200).json("Card ID " + req.body.id_card + " Não existe");
-                } else
+                } else if (rows.length > 0) {
                     res.status(201).json("Card ID " + req.body.id_card + " Atualizado");
+                } else {
+                    res.status(200).json("Card ID "+ req.body.id_card +" Não existe");
+                }
             }) 
-})
+});
 
 app.delete("/card", (req, res, next) => {
     db.all(`DELETE FROM card WHERE id_card=?`, 
             [
                 req.body.id_card
-            ], (err, rows) => {
+            ], function(err, rows){
                 if(err) {
-                    res.status(400).json({ "error": err.message })
+                    res.status(400).json({ "error": err.message });
                     return;
-                } else if (rows = undefined || []){
-                    res.status(200).json("Card ID " + req.body.id_card + " Não existe");
-                } else
+                } else { 
                     res.status(200).json("Card ID " + req.body.id_card + " excluida com sucesso");
+                }
             })
 });
 
 app.get("/game", (req, res, next) => {
-    const result = db.all(`SELECT CASE
-                            WHEN (SUM(one.hp + one.attack + one.defense + one.special_attack + one.special_defense + one.speed) >
-                                SUM(two.hp + two.attack + two.defense + two.special_attack + two.special_defense + two.speed))
-                            THEN 1
-                            WHEN (SUM(one.hp + one.attack + one.defense + one.special_attack + one.special_defense + one.speed) <
-                                SUM(two.hp + two.attack + two.defense + two.special_attack + two.special_defense + two.speed))
-                            THEN 2
-                            WHEN (SUM(one.hp + one.attack + one.defense + one.special_attack + one.special_defense + one.speed) =
-                                SUM(two.hp + two.attack + two.defense + two.special_attack + two.special_defense + two.speed))
-                            THEN 3
-                            ELSE 0
-                            END AS result
+    const result = db.all(`SELECT
+                            SUM(one.hp + one.attack + one.defense + one.special_attack + one.special_defense + one.speed) as one,
+                            SUM(two.hp + two.attack + two.defense + two.special_attack + two.special_defense + two.speed) as two
                             FROM card AS one, card AS two WHERE one.id_card=? AND two.id_card=?`, 
                             [
                                 req.body.playerOneCard,
                                 req.body.playerTwoCard
-                            ], (err, rows) => {
+                            ], function(err, rows){
                                 if (err) {
                                     res.status(400).json({ "error": err.message });
                                     return;
-                                } else if (rows[0].result === 3){
+                                } else if (rows[0].one > rows[0].two){
+                                    db.run(`INSERT INTO game (playeronecard,playertwocard,winner,loser)
+                                    VALUES (?,?,?,?)`,
+                                        [
+                                            req.body.playerOneCard,
+                                            req.body.playerTwoCard,
+                                            req.body.playerOneCard,
+                                            req.body.playerTwoCard
+                                        ], function(err, rows){
+                                            if (err) {
+                                                res.status(400).json({ error: err.message });
+                                                return;
+                                            } else {
+                                                db.all(`SELECT
+                                                b.winner, b.loser, a.hp, a.attack, a.defense, a.special_attack, a.special_defense, a.speed
+                                                FROM card AS a
+                                                JOIN game AS b ON b.winner=a.id_card
+                                                WHERE b.winner=? AND a.id_card=?
+                                                GROUP BY id_card`, 
+                                                    [
+                                                        req.body.playerOneCard,
+                                                        req.body.playerOneCard
+                                                    ], function(err, rows){
+                                                        if (err) {
+                                                            res.status(400).json({ "error": err.message });
+                                                            return;
+                                                        } else if (rows.length > 0) {
+                                                            res.status(200).json(rows);
+                                                        } else {
+                                                            res.status(200).json("Jogo não registrado");
+                                                        }
+                                                    });
+                                            }
+                                        }
+                                    );
+                                } else if (rows[0].one < rows[0].two){
+                                    db.run(`INSERT INTO game (playeronecard,playertwocard,winner,loser)
+                                        VALUES (?,?,?,?)`,
+                                        [
+                                            req.body.playerOneCard,
+                                            req.body.playerTwoCard,
+                                            req.body.playerTwoCard,
+                                            req.body.playerOneCard
+                                        ], function(err, rows){
+                                            if (err) {
+                                                res.status(400).json({ error: err.message });
+                                                return;
+                                            } else {
+                                                db.all(`SELECT
+                                                b.winner, b.loser, a.hp, a.attack, a.defense, a.special_attack, a.special_defense, a.speed
+                                                FROM card AS a
+                                                JOIN game AS b ON b.winner=a.id_card
+                                                WHERE b.winner=? AND a.id_card=?
+                                                GROUP BY id_card`, 
+                                                    [
+                                                        req.body.playerTwoCard,
+                                                        req.body.playerTwoCard
+                                                    ], function(err, rows){
+                                                        if (err) {
+                                                            res.status(400).json({ "error": err.message });
+                                                            return;
+                                                        } else if (rows.length > 0) {
+                                                            res.status(200).json(rows);
+                                                        } else {
+                                                            res.status(200).json("Jogo não registrado");
+                                                        }
+                                                    });
+                                            }
+                                        }
+                                    );
+                                } else if (rows[0].one && rows[0].two === null){
                                     res.status(200).json("empate");
-                                } else if (rows[0].result === 2){
-                                    res.status(200).json("Card 2 ganador");
-                                } else if (rows[0].result === 1){
-                                    res.status(200).json("Card 1ganador");
-                                }else
-                                    res.status(200).json("Card ID " + req.body.id_card + " Não existe");
+                                } else {
+                                    res.status(200).json("Card não identificada");
+                                }
                         });
+});
+
+app.get("/games", (req, res, next) => {
+    db.all(`SELECT COUNT(playeronecard) AS playerOne,
+	        COUNT(DISTINCT playeronecard) AS playerTwo
+            FROM game WHERE playeronecard=winner`,
+            [], function(err, rows){
+                if (err) {
+                    res.status(400).json({ "error": err.message });
+                    return;
+                } else if (rows.length > 0) {
+                    res.status(200).json(rows);
+                } else {
+                    res.status(200).json("Card ID "+ req.body.id_card +" Não existe");
+                }
+            });
 });
   
 app.listen(3001, () => {
